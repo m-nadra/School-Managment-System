f3b991bd45a036f2a95db3afa8ef580964487732
Mikolaj Nadra <m.nadra@proton.me>
2025-04-27 18:44:29 +0200

    setup cors

--- backend/src/routes/main.py
+++ backend/src/routes/main.py
@@ -3,6 +3,10 @@ from . import teacher
 from ..database import createTables
 from contextlib import asynccontextmanager
 from prometheus_client import make_asgi_app
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+FRONTEND_URL : str = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +17,13 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(teacher)
 
 app.mount("/metrics", make_asgi_app(), name="metrics")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
