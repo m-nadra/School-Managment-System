ee20025a326862924b3e6e66cadedae6d6451b19
Mikolaj Nadra <m.nadra@proton.me>
2025-05-10 17:57:51 +0200

    remove app.tsx file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from '@/components/ui/provider'
-import App from './App.tsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Main from "./pages/Main";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-    <App />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />}/>
+        </Routes>
+      </Router>
     </Provider>
   </StrictMode>,
 )
