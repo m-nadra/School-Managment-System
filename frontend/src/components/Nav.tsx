168e28482314dc64ed9ca7f60510111eb61039e6
Mikolaj Nadra <m.nadra@proton.me>
2025-05-19 15:56:27 +0200

    frontend: add links in nav to other endpoints

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -2,12 +2,14 @@ import {
     Flex, 
     IconButton,
     Text, 
-
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useColorMode, useColorModeValue } from "../components/ui/color-mode";
 import { LuMoon, LuSun, LuLogOut } from "react-icons/lu";
+import { GiTeacher } from "react-icons/gi";
+import { CgProfile } from "react-icons/cg";
+import { Link } from "react-router-dom";
 
 type User = {
     username: string;
@@ -42,12 +44,22 @@ export default function Nav() {
         })
     }, []);
     return (
-        <Flex bg={useColorModeValue("gray.100", "gray.900")} width="15%" height="100vh" flexDirection="column" justifyContent="space-between">
+        <Flex bg={useColorModeValue("gray.100", "gray.900")} width="15%" height="100vh" flexDirection="column" gap="1" justify="start">
             <IconButton onClick={toggleColorMode} variant="ghost" size="md">
                 {colorMode === "light" ? <LuSun /> : <LuMoon />}
             </IconButton>
-            <Flex alignSelf="flex-end" bg={useColorModeValue("gray.200", "gray.800")}
-            w="100%" justify="space-between" p="3" borderRadius="lg" justifySelf="center" align="center">
+            <Link to="profile">
+                <Flex bg={useColorModeValue("gray.200", "gray.800")} p="2" gap="2" align="center" borderRadius="lg">
+                    <CgProfile/><Text fontSize="lg">Profile</Text>
+                </Flex>
+            </Link>
+            <Link to="teachers">
+                <Flex bg={useColorModeValue("gray.200", "gray.800")} p="2" gap="2" align="center" borderRadius="lg">
+                    <GiTeacher/><Text fontSize="lg">Teachers</Text>
+                </Flex>
+            </Link>
+            <Flex justifySelf="end" bg={useColorModeValue("gray.200", "gray.800")} mt="auto"
+            w="100%" justify="space-between" p="3" borderRadius="lg" align="center">
                 <Flex flexDirection="column">
                     <Text fontSize="md" fontWeight="bold">{user ? user.username.toUpperCase() : ""}</Text>
                     <Text fontSize="sm">{user ? `Role: ${user.role.toUpperCase()}` : ""}</Text>
