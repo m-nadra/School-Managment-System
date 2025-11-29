da3aa4ae9242b8121de6174ae82a646511f9b25c
Mikolaj Nadra <m.nadra@proton.me>
2025-11-29 22:21:58 +0100

    created compose watch, removed prometheus

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,8 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0',
+    port: 80
+  }
 })
